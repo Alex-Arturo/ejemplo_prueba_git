1832b5df8a2071d1b718911657ca87d16d1427d6
Alex <alexarturo0910@gmail.com>
2025-11-29 19:49:11 -0600

    Nueva implementacion ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -3,5 +3,13 @@ nombre_completo = ["Alex", "Arturo","Vazquez","Ricardez"]
 for partes_nombre in nombre_completo:
     print(partes_nombre)
 
-
 print("Gracias por colaborar - Chase")
+
+def restar(num, num2):
+    return num-num2
+
+def sumar(num, num2):
+    return num+num2
+
+
+print("Nueva funcion agregada")
